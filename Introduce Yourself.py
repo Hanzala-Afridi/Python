3a44bf2de4af2715624bbcdaaf63ccd3e07bc076
Hanzala-Afridi <hanzalaafridi12345@gmail.com>
2024-09-01 22:58:05 +0500

    Add files via upload

---
+++ Introduce Yourself.py
@@ -0,0 +1,16 @@
+# Introduce Yourself
+
+Name = input("Enter Your Name: ")
+Age = int(input("Enter Your Age: "))
+Qualification = input("Enter Your Qualification: ")
+Experience = input("Enter Your Experience: ")
+Skills = input("Enter Your Skills: ")
+
+print("My Name is:", Name)
+print("My age is:", Age)
+print("My Qualification:", Qualification)
+print("My Experience:", Experience)
+print("My Skills:", Skills)
+
+# Print the final sentence
+print("Don't mention anything else besides this.")
