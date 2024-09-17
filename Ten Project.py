9c65a3d581daf97625470a968d19b114fcafad23
Hanzala-Afridi <hanzalaafridi12345@gmail.com>
2024-09-17 21:43:16 +0500

    Add files via upload

---
+++ Ten Project.py
@@ -0,0 +1,8 @@
+Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+index = 0
+
+while (index < len(Number)):
+    if (Number[index] % 2 == 0):
+        print(Number[index])
+        
+    index = index + 1
